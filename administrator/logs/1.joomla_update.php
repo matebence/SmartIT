b13ddd967ba27281795729d706208d43c4ae0171
Bence <m.bence05@gmail.com>
2018-11-23 19:12:22 +0100

    joomla 3.9.0 update

---
+++ administrator/logs/1.joomla_update.php
@@ -0,0 +1,60 @@
+#
+#<?php die('Forbidden.'); ?>
+#Date: 2018-07-03 17:52:55 UTC
+#Software: Joomla Platform 13.1.0 Stable [ Curiosity ] 24-Apr-2013 00:00 GMT
+
+#Fields: datetime	priority clientip	category	message
+2018-07-03T17:52:55+00:00	INFO ::1	update	Update started by user Jones (45). Old version is 3.8.5.
+2018-07-03T17:53:00+00:00	INFO ::1	update	Downloading update file from https://s3-us-west-2.amazonaws.com/joomla-official-downloads/joomladownloads/joomla3/Joomla_3.8.10-Stable-Update_Package.zip?X-Amz-Algorithm=AWS4-HMAC-SHA256&X-Amz-Credential=AKIAIZ6S3Q3YQHG57ZRA%2F20180703%2Fus-west-2%2Fs3%2Faws4_request&X-Amz-Date=20180703T175257Z&X-Amz-Expires=60&X-Amz-SignedHeaders=host&X-Amz-Signature=0a8381338c97c95e7b3e38cede06fce96a6d7fb6c2ca64e6bdf680ca29f29cb7.
+2018-07-03T17:53:48+00:00	INFO ::1	update	File Joomla_3.8.10-Stable-Update_Package.zip downloaded.
+2018-07-03T17:53:48+00:00	INFO ::1	update	Starting installation of new version.
+2018-07-03T17:54:50+00:00	INFO ::1	update	Finalising installation.
+2018-07-03T17:54:51+00:00	INFO ::1	update	Ran query from file 3.8.6-2018-02-14. Query text: INSERT INTO `#__extensions` (`extension_id`, `package_id`, `name`, `type`, `elem.
+2018-07-03T17:54:51+00:00	INFO ::1	update	Ran query from file 3.8.6-2018-02-14. Query text: INSERT INTO `#__postinstall_messages` (`extension_id`, `title_key`, `description.
+2018-07-03T17:54:51+00:00	INFO ::1	update	Ran query from file 3.8.8-2018-05-18. Query text: INSERT INTO `#__postinstall_messages` (`extension_id`, `title_key`, `description.
+2018-07-03T17:54:51+00:00	INFO ::1	update	Ran query from file 3.8.9-2018-06-19. Query text: UPDATE `#__extensions` SET `enabled` = '1' WHERE `name` = 'mod_sampledata';.
+2018-07-03T17:54:51+00:00	INFO ::1	update	Deleting removed files and folders.
+2018-07-03T17:55:00+00:00	INFO ::1	update	Cleaning up after installation.
+2018-07-03T17:55:00+00:00	INFO ::1	update	Update to version 3.8.10 is complete.
+2018-11-23T18:07:57+00:00	INFO ::1	update	Starting installation of new version.
+2018-11-23T18:08:25+00:00	INFO ::1	update	Finalising installation.
+2018-11-23T18:08:25+00:00	INFO ::1	update	Ran query from file 3.9.0-2018-05-02. Query text: INSERT INTO `#__extensions` (`extension_id`, `package_id`, `name`, `type`, `elem.
+2018-11-23T18:08:26+00:00	INFO ::1	update	Ran query from file 3.9.0-2018-05-02. Query text: CREATE TABLE IF NOT EXISTS `#__privacy_requests` (   `id` int(10) unsigned NOT N.
+2018-11-23T18:08:26+00:00	INFO ::1	update	Ran query from file 3.9.0-2018-05-03. Query text: INSERT INTO `#__extensions` (`extension_id`, `package_id`, `name`, `type`, `elem.
+2018-11-23T18:08:26+00:00	INFO ::1	update	Ran query from file 3.9.0-2018-05-05. Query text: INSERT INTO `#__extensions` (`extension_id`, `package_id`, `name`, `type`, `elem.
+2018-11-23T18:08:26+00:00	INFO ::1	update	Ran query from file 3.9.0-2018-05-05. Query text: CREATE TABLE IF NOT EXISTS `#__action_logs` (   `id` int(10) unsigned NOT NULL A.
+2018-11-23T18:08:26+00:00	INFO ::1	update	Ran query from file 3.9.0-2018-05-05. Query text: CREATE TABLE IF NOT EXISTS `#__action_logs_extensions` (   `id` int(10) unsigned.
+2018-11-23T18:08:26+00:00	INFO ::1	update	Ran query from file 3.9.0-2018-05-05. Query text: INSERT INTO `#__action_logs_extensions` (`id`, `extension`) VALUES (1, 'com_bann.
+2018-11-23T18:08:27+00:00	INFO ::1	update	Ran query from file 3.9.0-2018-05-05. Query text: CREATE TABLE IF NOT EXISTS `#__action_log_config` (   `id` int(10) unsigned NOT .
+2018-11-23T18:08:27+00:00	INFO ::1	update	Ran query from file 3.9.0-2018-05-05. Query text: INSERT INTO `#__action_log_config` (`id`, `type_title`, `type_alias`, `id_holder.
+2018-11-23T18:08:27+00:00	INFO ::1	update	Ran query from file 3.9.0-2018-05-19. Query text: INSERT INTO `#__extensions` (`extension_id`, `package_id`, `name`, `type`, `elem.
+2018-11-23T18:08:27+00:00	INFO ::1	update	Ran query from file 3.9.0-2018-05-20. Query text: INSERT INTO `#__extensions` (`extension_id`, `package_id`, `name`, `type`, `elem.
+2018-11-23T18:08:27+00:00	INFO ::1	update	Ran query from file 3.9.0-2018-05-24. Query text: INSERT INTO `#__extensions` (`extension_id`, `package_id`, `name`, `type`, `elem.
+2018-11-23T18:08:28+00:00	INFO ::1	update	Ran query from file 3.9.0-2018-05-24. Query text: CREATE TABLE IF NOT EXISTS `#__privacy_consents` (   `id` int(10) unsigned NOT N.
+2018-11-23T18:08:28+00:00	INFO ::1	update	Ran query from file 3.9.0-2018-05-27. Query text: INSERT INTO `#__extensions` (`extension_id`, `package_id`, `name`, `type`, `elem.
+2018-11-23T18:08:29+00:00	INFO ::1	update	Ran query from file 3.9.0-2018-06-02. Query text: ALTER TABLE `#__content` ADD COLUMN `note` VARCHAR(255) NOT NULL DEFAULT '';.
+2018-11-23T18:08:29+00:00	INFO ::1	update	Ran query from file 3.9.0-2018-06-02. Query text: UPDATE `#__content_types` SET `field_mappings` =  '{"common":{"core_content_item.
+2018-11-23T18:08:29+00:00	INFO ::1	update	Ran query from file 3.9.0-2018-06-12. Query text: INSERT INTO `#__extensions` (`extension_id`, `package_id`, `name`, `type`, `elem.
+2018-11-23T18:08:29+00:00	INFO ::1	update	Ran query from file 3.9.0-2018-06-13. Query text: INSERT INTO `#__extensions` (`extension_id`, `package_id`, `name`, `type`, `elem.
+2018-11-23T18:08:29+00:00	INFO ::1	update	Ran query from file 3.9.0-2018-06-14. Query text: INSERT INTO `#__postinstall_messages` (`extension_id`, `title_key`, `description.
+2018-11-23T18:08:29+00:00	INFO ::1	update	Ran query from file 3.9.0-2018-06-17. Query text: INSERT INTO `#__extensions` (`extension_id`, `package_id`, `name`, `type`, `elem.
+2018-11-23T18:08:29+00:00	INFO ::1	update	Ran query from file 3.9.0-2018-07-09. Query text: INSERT INTO `#__extensions` (`extension_id`, `package_id`, `name`, `type`, `elem.
+2018-11-23T18:08:29+00:00	INFO ::1	update	Ran query from file 3.9.0-2018-07-10. Query text: INSERT INTO `#__action_log_config` (`id`, `type_title`, `type_alias`, `id_holder.
+2018-11-23T18:08:29+00:00	INFO ::1	update	Ran query from file 3.9.0-2018-07-11. Query text: INSERT INTO `#__extensions` (`extension_id`, `package_id`, `name`, `type`, `elem.
+2018-11-23T18:08:30+00:00	INFO ::1	update	Ran query from file 3.9.0-2018-08-12. Query text: ALTER TABLE `#__privacy_consents` ADD COLUMN `state` INT(10) NOT NULL DEFAULT '1.
+2018-11-23T18:08:31+00:00	INFO ::1	update	Ran query from file 3.9.0-2018-08-28. Query text: ALTER TABLE `#__session` MODIFY `session_id` varbinary(192) NOT NULL;.
+2018-11-23T18:08:31+00:00	INFO ::1	update	Ran query from file 3.9.0-2018-08-28. Query text: ALTER TABLE `#__session` MODIFY `guest` tinyint(3) unsigned DEFAULT 1;.
+2018-11-23T18:08:32+00:00	INFO ::1	update	Ran query from file 3.9.0-2018-08-28. Query text: ALTER TABLE `#__session` MODIFY `time` int(11) NOT NULL DEFAULT 0;.
+2018-11-23T18:08:32+00:00	INFO ::1	update	Ran query from file 3.9.0-2018-08-29. Query text: INSERT INTO `#__extensions` (`extension_id`, `package_id`, `name`, `type`, `elem.
+2018-11-23T18:08:32+00:00	INFO ::1	update	Ran query from file 3.9.0-2018-09-04. Query text: CREATE TABLE IF NOT EXISTS `#__action_logs_users` (   `user_id` int(11) UNSIGNED.
+2018-11-23T18:08:32+00:00	INFO ::1	update	Ran query from file 3.9.0-2018-10-15. Query text: ALTER TABLE `#__action_logs` ADD INDEX `idx_user_id` (`user_id`);.
+2018-11-23T18:08:33+00:00	INFO ::1	update	Ran query from file 3.9.0-2018-10-15. Query text: ALTER TABLE `#__action_logs` ADD INDEX `idx_user_id_logdate` (`user_id`, `log_da.
+2018-11-23T18:08:33+00:00	INFO ::1	update	Ran query from file 3.9.0-2018-10-15. Query text: ALTER TABLE `#__action_logs` ADD INDEX `idx_user_id_extension` (`user_id`, `exte.
+2018-11-23T18:08:33+00:00	INFO ::1	update	Ran query from file 3.9.0-2018-10-15. Query text: ALTER TABLE `#__action_logs` ADD INDEX `idx_extension_item_id` (`extension`, `it.
+2018-11-23T18:08:33+00:00	INFO ::1	update	Ran query from file 3.9.0-2018-10-20. Query text: ALTER TABLE `#__privacy_requests` DROP INDEX `idx_checkout`;.
+2018-11-23T18:08:34+00:00	INFO ::1	update	Ran query from file 3.9.0-2018-10-20. Query text: ALTER TABLE `#__privacy_requests` DROP COLUMN `checked_out`;.
+2018-11-23T18:08:34+00:00	INFO ::1	update	Ran query from file 3.9.0-2018-10-20. Query text: ALTER TABLE `#__privacy_requests` DROP COLUMN `checked_out_time`;.
+2018-11-23T18:08:34+00:00	INFO ::1	update	Ran query from file 3.9.0-2018-10-21. Query text: INSERT INTO `#__extensions` (`extension_id`, `package_id`, `name`, `type`, `elem.
+2018-11-23T18:08:35+00:00	INFO ::1	update	Deleting removed files and folders.
+2018-11-23T18:08:50+00:00	INFO ::1	update	Cleaning up after installation.
+2018-11-23T18:08:50+00:00	INFO ::1	update	Update to version 3.9.0 is complete.
